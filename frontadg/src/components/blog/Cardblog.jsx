9ad0cc4eadb9fa2078b619e1be380686f777a07b
blanco <mblancospadaro@gmail.com>
2022-11-04 16:58:48 +0100

    imagenes/secciones

---
+++ frontadg/src/components/blog/Cardblog.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import image1 from "../../assets/image1.jpg";
+
+
+function Cardblog({title,imageUrl,body}) {
+  return (
+
+    
+
+    
+    <section className="blog">
+  
+    <div className= "card-container">
+        <div className= "image-container">
+            <img src={image1} alt=""/>
+        </div>
+        <div className= "card-content">
+
+        </div>
+        <div className= "card-title">
+            <h3>Alimentación</h3>
+        </div>
+        <div className= "card-body">
+            <p>Se recomienda que los nutrientes para las  personas con diabetes sea la misma que para la población general, atendiendo la situación 
+      clínica de cada paciente, individualizándose su composición en cada ingesta"</p>
+        </div>
+        <div className= "btn">
+            <button>
+                <a href="/"> Leer más</a>
+            </button>
+        </div>
+    </div>
+    </section>
+  
+    
+  )
+};
+
+export default Cardblog;
