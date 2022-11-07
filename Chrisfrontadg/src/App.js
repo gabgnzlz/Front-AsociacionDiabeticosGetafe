3f7ac8a396c7bda967bfd929e9d5fe4e8b27b0e4
christinap <christypm24@gmail.com>
2022-11-07 09:55:37 +0100

    getaf

---
+++ Chrisfrontadg/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import Cardblog from './components/blog/Cardblog';
+import './components/blog/cardblog.css';
+import Blog from './components/blog/Blog';
+import './components/blog/Blog.css';
+
+function App() {
+  return (
+
+
+    <div className="App">
+    <Blog/>
+    <Cardblog/>
+      </div>
+
+
+  );
+}
+
+export default App;
---
+++ Chrisfrontadg/src/components/Contacto/Contacto.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+function Contacto() {
+  return (
+    <section>
+    
+    <div className="inicioContacto">
+      <h1>CONTÁCTANOS</h1>
+
+      </div>
+
+      
+  </section>
+ 
+  
+
+  
+
+  )
+}
+
+export default Contacto
