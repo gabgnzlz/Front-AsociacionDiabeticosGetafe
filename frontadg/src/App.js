2468093f4ee53078f7b4b1554efefde80557bf87
blanco <mblancospadaro@gmail.com>
2022-11-15 16:50:34 +0100

    formularios

--- frontadg/src/App.js
+++ frontadg/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
-
+import Contacto from './components/formularios/form_contacto/Contacto';
 
 function App() {
   return (
 
 
     <>
+<Contacto/>
 
     </>
 
