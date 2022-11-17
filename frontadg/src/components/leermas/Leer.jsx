622410b8e9bb8ac59206c7cea8e505bf163fce5b
blanco <mblancospadaro@gmail.com>
2022-11-17 19:07:56 +0100

    leermas

---
+++ frontadg/src/components/leermas/Leer.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import leer from "../assets/leer.jpg";
+
+function Leer() {
+  return (
+
+    <section className="read">
+    <div className="leer_mas">
+        <img src={leer} alt="leer"/>
+    </div>
+
+    
+    <div className= "title">
+         <h3>Vida saludable</h3>
+    </div>
+    
+    <div className= "text">
+    <p> Algunas personas con diabetes tienen que comer casi a la misma hora todos los días. Otras pueden ser más flexibles con el horario de sus comidas. 
+    Dependiendo de sus medicinas para la 
+    diabetes o el tipo de insulina que use, es posible que tenga que consumir la misma cantidad de carbohidratos todos los días a la misma hora. 
+    Si usted usa insulina “a la hora de comer”, 
+    sus horarios de comidas pueden ser más flexibles.
+
+    Si usa insulina o ciertas medicinas para la diabetes y omite o retrasa una comida, su nivel de glucosa en la sangre puede bajar demasiado.
+    Pregúntele a su equipo de atención médica 
+    cuándo debe comer y si debe hacerlo antes y después de hacer una actividad física.
+    Si usted tiene sobrepeso o tiene obesidad, trabaje con su equipo de atención médica para establecer un plan para perder peso.
+    El planificador para adelgazar puede ayudarle a adaptar sus planes de actividad física y planes de calorías para alcanzar y mantener su peso deseado.
+    Para adelgazar es necesario usted tiene que consumir menos calorías y reemplazar los alimentos menos saludables por alimentos bajos en calorías, grasas y azúcar.
+<br>
+</br>
+<br>
+
+</br>
+    Si usted tiene diabetes, sobrepeso u obesidad y está pensando en tener un bebé, debe tratar de perder el exceso de peso antes de quedar embarazada. Aprenda más sobre el embarazo y diabetes.
+    El método del plato le ayuda a controlar el tamaño de sus porciones. No tiene que contar calorías. El método le muestra la cantidad de cada grupo de alimentos que debe comer y funciona 
+    mejor para el almuerzo y la cena.
+
+    Utilice un plato de 9 pulgadas (15 cm) de diámetro. Llene la mitad del plato con las verduras sin almidón, en un cuarto del plato coloque una carne u otra proteína y en el último cuarto 
+    del plato ponga los granos u otro tipo de fuente de almidón. Las fuentes de almidón incluyen verduras feculentas como el maíz y los guisantes. También puede comer una taza pequeña de fruta 
+    o un pedazo de fruta y tomar un vaso pequeño de leche si está incluido en su plan de alimentación.
+    </p>
+
+    </div>
+    
+    </section>
+
+
+  )
+}
+
+export default Leer
