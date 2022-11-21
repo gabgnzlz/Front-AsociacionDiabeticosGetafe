5255260f3df4722f52f4bd620e71a926e73bdc7a
Alexandra Zambrano <alezambrano03@gmail.com>
2022-11-21 10:19:21 +0100

    revistas

--- frontadg/src/App.js
+++ frontadg/src/App.js
@@ -5,6 +5,7 @@ import HomePage from './components/pageConect/Homepage';
 import Footer from './components/footer/Footer';
 import Aboutuspage from './components/pageConect/Aboutuspage';
 import BlogNoticias from './components/pageConect/BlogNoticias';
+import RevConect from './components/pageConect/RevConect';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path='/' exact component={HomePage} />
           <Route path='/nosotros' exact component={Aboutuspage} />
           <Route path='/blog' exact component={BlogNoticias} />
+          <Route path='/revista' exact component={RevConect} />
         </Switch>
         <Footer />
       </Router>
---
+++ frontadg/src/components/pageConect/RevConect.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Revistas from '../revistas/Revistas'
+import Cardrevistas from '../revistas/Cardrevistas'
+
+function RevConect() {
+  return (
+    <div>
+      <Revistas />
+      <Cardrevistas />
+    </div>
+  )
+}
+
+export default RevConect
